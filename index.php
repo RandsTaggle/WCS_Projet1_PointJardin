830f6b99befcdbd05421eaf809ed9cac536200e2
RandsTaggle <cyril.lemaire@wanadoo.fr>
2016-09-07 14:30:29 +0200

    tests ongoing

--- index.php
+++ index.php
@@ -1,12 +1,18 @@
-<!-- HTML5 -->
 <head>
-	<stylesheet >
+	<link rel="StyleSheet" href="sources/materialize.min.css" type="text/css"></link>
+	<link rel="StyleSheet" href="sources/style.css" type="text/css"></link>
+	<title>Point Jardin ǀ Matériaux pour le Jardin ǀ Saint Rémy sur Avre</title>
 </head>
 <body>
 	<header>
+		<div class="container">
+			<div class="grey_header_bar">
+				<p>Header grey bar</p>
+			</div>
+		</div>
 	</header>
-	<div>		<!-- Empty div -->c
-	</div>
+		<div>	<!-- Empty div -->
+		</div>
 	<div>		<!-- Main div -->
 	</div>
 	<div>		<!-- Social networks slider -->
