abaf29c9607bd5734287c388103b739e7dc6733c
RandsTaggle <cyril.lemaire@wanadoo.fr>
2016-09-16 02:20:37 +0200

    Merged Pascal's work

---
+++ includes/header.php
@@ -0,0 +1,33 @@
+<header class="">
+
+    <div class="container-fluid">
+        <nav class="light-green darken-1" role="navigation">
+            <div class="nav-wrapper">
+                <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+                <ul class="side-nav" id="mobile-demo">
+                     <li><a href="#">Accueil</a></li>
+                        <li><a href="#">Mineral</a></li>
+                        <li><a href="#">Végétal</a></li>
+                        <li><a href="#">Accessoires</a></li>
+                        <li><a href="#">Nos services</a></li>
+                        <li><a href="#">Conception</a></li>    
+                        <li><a href="#">Qui sommes nous</a></li>
+                </ul>
+                <div class="navbar-center">
+                    <ul id="nav-mobile" class="hide-on-med-and-down">
+                        <li><a href="#">Accueil</a></li>
+                        <li><a href="#">Mineral</a></li>
+                        <li><a href="#">Végétal</a></li>
+                        <li><a href="#">Accessoires</a></li>
+                        <li><a href="#">Nos services</a></li>
+                        <li><a href="#">Conception</a></li>    
+                        <li><a href="#">Qui sommes nous</a></li>
+                    </ul>
+                </div>
+
+            </div>
+    </div>
+</nav>
+
+</div>
+</header>
--- index.php
+++ index.php
@@ -1,16 +1,45 @@
-<!-- HTML5 -->
-<head>
-	<stylesheet >
-</head>
-<body>
-	<header>
-	</header>
-	<div>		<!-- Empty div -->c
-	</div>
-	<div>		<!-- Main div -->
-	</div>
-	<div>		<!-- Social networks slider -->
-	</div>
-	<footer>	<!-- Copyrights -->
-	</footer>
-</body>
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <title>point jardin</title>
+        <meta charset="UTF-8">
+        <meta name="point-jardin" content="" />
+        
+        <!-- Compiled and minified CSS -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/css/materialize.min.css">
+        <!--Import Google Icon Font-->
+        <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <!--Import materialize.css-->
+        <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+        <link rel="stylesheet" href="css/style.css" />
+
+    </head>
+    <body>
+
+
+        <?php
+        // contains header nav
+        require_once 'includes/header.php';
+        ?>
+
+        <main role="main">
+            
+        </main>
+
+
+        <?php
+        // footer and scripts
+        require_once('includes/footer.php');
+        ?>
+
+
+
+
+
+    </body>
+</html>
